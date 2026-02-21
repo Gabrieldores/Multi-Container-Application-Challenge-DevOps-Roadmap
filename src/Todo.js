b85f22942b68227d9658e237322803d7c406926e
Gabriel Lucas <gabriellucas.dores@gmail.com>
2026-02-21 16:02:22 -0300

    Primeiro commit: subindo código existente

---
+++ src/Todo.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, "Por favor, adicione um Título"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Por favor, adicione uma descrição"],
+        trim: true,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
